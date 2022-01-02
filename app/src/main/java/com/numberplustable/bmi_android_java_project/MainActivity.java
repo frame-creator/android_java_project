0efc8569c932d18418b2fb73175a3b90fb26cf81
code-framer <journeyofman@naver.com>
2022-01-03 08:13:30 +0900

    RelativeLayout 성별 고르기 ui 추가

---
+++ app/src/main/java/com/numberplustable/bmi_android_java_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.numberplustable.bmi_android_java_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
